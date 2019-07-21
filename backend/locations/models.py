81235a1e7261e883c329dd5e35edd9e1a2cb52af
Artem Shlapakov <xpressmusik@mail.ru>
2019-07-21 14:42:32 +0300

    Locations model

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{} city".format(self.name)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.ForeignKey(Country,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, Country.name)
