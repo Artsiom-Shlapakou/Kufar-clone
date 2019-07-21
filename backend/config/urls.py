b28b04ef9e826235801223105a747bc43875ba0b
Artem Shlapakov <xpressmusik@mail.ru>
2019-07-21 14:46:52 +0300

    Add apipatterns to config.urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,4 @@
 """config URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -13,9 +12,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
+apipatterns = [
+    path('', include('authentication.urls')),
+    path('', include('locations.urls')),
+    path('', include('companies.urls')),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/', include((apipatterns, 'api'), namespace='api'))
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
