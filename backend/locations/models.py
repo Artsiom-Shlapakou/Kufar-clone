e7bcc6cb38799e018790f19013e5c28fdd3ea1cd
Artem Shlapakov <xpressmusik@mail.ru>
2019-07-24 12:38:28 +0300

    fixed bags

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
+    country_id = models.CharField(primary_key=True, unique=True, max_length=10)
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -12,8 +13,11 @@ class Country(models.Model):
 
 
 class City(models.Model):
+    city_id = models.CharField(primary_key=True, unique=True, max_length=10)
     name = models.CharField(max_length=50)
     country = models.ForeignKey(Country,on_delete=models.CASCADE)
+
+    parent_id = models.ForeignKey(to='self', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return "{} city, {}".format(self.name, self.country.name)
---
+++ backend/locations/services.py
@@ -0,0 +1,49 @@
+import requests
+from .models import Country, City
+
+
+class GeoLocationApiClient:
+
+    BASE_URL = "https://api.hh.ru"
+    ALL_COUNTRIES_URL = '/areas/countries'
+    CITIES_FROM_COUNTRY_URL = '/areas/{0}'
+
+    @classmethod
+    def get_all_countries(cls):
+        response = requests.get(cls.BASE_URL + cls.ALL_COUNTRIES_URL)
+        data = response.json()
+        return data
+
+    @classmethod
+     def get_all_cities_from_country(cls, id):
+        response = requests.get(cls.BASE_URL + cls.CITIES_FROM_COUNTRY_URL.format(id))
+        data = response.json()
+        return data['areas']
+
+
+class DataHandler:
+
+    def save_countries_list(self, countries):
+        for country in countries:
+            Country.objects.get_or_create(
+                country_id=country['id'],
+                name=country['name'],
+            )
+            
+    def save_cities_list(self, cities):
+        for city in cities:
+            City.objects.get_or_create(
+                city_id=city['id'],
+                name=city['name'],
+                country_id=city['parent_id']
+            )
+    
+
+def load_locations():
+    g = GeoLocationApiClient()
+    data_handler = DataHandler()
+    countries = g.get_all_countries()
+    data_handler.save_countries_list(countries)
+    for country in countries:
+        cities = g.get_all_cities_from_country(country['id'])
+        data_handler.save_cities_list(cities)
